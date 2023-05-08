1f558309beb94902a4ff57c2d38ad5cca126741d
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-05-08 16:09:17 -0300

    resolução dos exercícios em C#

---
+++ listaUm.cs
@@ -0,0 +1,12 @@
+﻿//0001
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int x = a+b;
+Console.WriteLine($"X = {x}");
+//0002
+//0004
+//0005
+//0007
+//0008
+//0009
+//0010
